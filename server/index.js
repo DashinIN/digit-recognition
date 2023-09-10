75c95294c234d4965a54cf4177b18fec7cd1db0c
DashinIN <green19grant@yandex.ru>
2023-09-11 00:20:41 +0300

    now it should work finaly

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ async function preprocessImage(imageBuffer) {
 
 app.use('/model', express.static(path.join(__dirname, 'model')));
 
-const modelPath = '/model/model.json'
+const modelPath = 'https://digit-recognition-api.onrender.com/model/model.json'
 
 app.post('/classify', upload.single('image'), async (req, res) => {
   try {
